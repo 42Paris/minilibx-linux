227dba193ca1b0bc540e82350d8520c10a37fc7b
Alexander Chetverkin <virtual-ins@mail.ru>
2020-08-31 01:20:52 +0300

    [FIX] Add missing function declarations and headers.

--- mlx.h
+++ mlx.h
@@ -74,7 +74,7 @@ char	*mlx_get_data_addr(void *img_ptr, int *bits_per_pixel,
 */
 int	mlx_put_image_to_window(void *mlx_ptr, void *win_ptr, void *img_ptr,
 				int x, int y);
-unsigned int	mlx_get_color_value(void *mlx_ptr, int color);
+int	mlx_get_color_value(void *mlx_ptr, int color);
 
 
 /*
