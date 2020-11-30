2bfd84c2fe5b85b79b9afec7591eec5e19dc90fc
lsoulier42 <louisesoulier789@gmail.com>
2020-11-30 12:50:38 +0100

    add mlx_loop_end

--- mlx_loop.c
+++ mlx_loop.c
@@ -28,40 +28,32 @@ static int	win_count(t_xvar *xvar)
 	return (i);
 }
 
+int			mlx_loop_end(t_xvar *xvar)
+{
+	xvar->end_loop = 1;
+	return (1);
+}
+
 int			mlx_loop(t_xvar *xvar)
 {
 	XEvent		ev;
 	t_win_list	*win;
-	int 		end_loop;
 
 	mlx_int_set_win_event_mask(xvar);
 	xvar->do_flush = 0;
-	end_loop = 0;
-	while (win_count(xvar))
+	while (win_count(xvar) && !xvar->end_loop)
 	{
-		while (!xvar->loop_hook || XPending(xvar->display))
+		while (!xvar->end_loop && (!xvar->loop_hook || XPending(xvar->display)))
 		{
 			XNextEvent(xvar->display,&ev);
 			win = xvar->win_list;
 			while (win && (win->window!=ev.xany.window))
 				win = win->next;
 			if (win && ev.type < MLX_MAX_EVENT && win->hooks[ev.type].hook)
-			{
-				end_loop = (ev.type == ClientMessage
-							&& (Atom)ev.xclient.data.l[0] == xvar->wm_delete_window)
-						   || ev.xkey.keycode == 9;
 				mlx_int_param_event[ev.type](xvar, &ev, win);
-				if (end_loop)
-					break;
-			}
 		}
-		if (!end_loop)
-		{
-			xvar->loop_hook(xvar->loop_param);
-			XSync(xvar->display, False);
-		}
-		else
-			break ;
+		xvar->loop_hook(xvar->loop_param);
+		XSync(xvar->display, False);
 	}
 	return (0);
 }
