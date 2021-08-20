0a6c78b0a96e420bef66bd2287b5534dbd0dcbb0
jodufour <jodufour@student.42.fr>
2021-08-20 20:21:12 +0200

    Debug

--- srcs/mlx_xpm/mlx_int_parse_xpm.c
+++ srcs/mlx_xpm/mlx_int_parse_xpm.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/05 17:35:45 by jodufour          #+#    #+#             */
-/*   Updated: 2021/08/20 18:55:13 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/08/20 20:20:44 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		mlx_int_get_text_rgb(char *name, char *end);
 int		part0(t_zeta *zeta);
 int		part1(t_zeta *zeta);
 int		part2(t_zeta *zeta);
-int		part3(t_zeta *zeta);
+int		part3(t_xvar *xvar, t_zeta *zeta);
 
 void	*mlx_int_parse_xpm(t_xvar *xvar, void *info, int info_size,
 	char *(*f)())
@@ -36,7 +36,7 @@ void	*mlx_int_parse_xpm(t_xvar *xvar, void *info, int info_size,
 		return (mlx_return(xvar, &zeta));
 	if (part2(&zeta) == -1)
 		return (mlx_return(xvar, &zeta));
-	if (part3(&zeta) == -1)
+	if (part3(xvar, &zeta) == -1)
 		return (mlx_return(xvar, &zeta));
 	if (zeta.colors)
 		free(zeta.colors);
