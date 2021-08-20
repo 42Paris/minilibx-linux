d5e0fc76a4a2167c302e5515264e5c4cb8a021a8
jodufour <jodufour@student.42.fr>
2021-08-20 18:12:16 +0200

    Fixed my mistake in int_file_get_rid_comment. Created an endless loop (oups)

--- srcs/mlx_xpm/mlx_int_file_get_rid_comment.c
+++ srcs/mlx_xpm/mlx_int_file_get_rid_comment.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/05 17:38:24 by jodufour          #+#    #+#             */
-/*   Updated: 2021/08/05 17:54:56 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/08/20 18:09:52 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 void	mlx_int_file_get_rid_comment(char *ptr, int size)
 {
-	int	com_begin;
+	int	com_beg;
 	int	com_end;
 
-	com_begin = mlx_int_str_str_cote(ptr, "/*", size);
-	while (com_begin != -1)
+	com_beg = mlx_int_str_str_cote(ptr, "/*", size);
+	while (com_beg != -1)
 	{
-		com_end = mlx_int_str_str(ptr + com_begin + 2, "*/",
-				size - com_begin - 2);
-		memset(ptr + com_begin, ' ', com_end + 4);
+		com_end = mlx_int_str_str(ptr + com_beg + 2, "*/", size - com_beg - 2);
+		memset(ptr + com_beg, ' ', com_end + 4);
+		com_beg = mlx_int_str_str_cote(ptr, "/*", size);
 	}
-	com_begin = mlx_int_str_str_cote(ptr, "//", size);
-	while (com_begin != -1)
+	com_beg = mlx_int_str_str_cote(ptr, "//", size);
+	while (com_beg != -1)
 	{
-		com_end = mlx_int_str_str(ptr + com_begin + 2, "\n",
-				size - com_begin - 2);
-		memset(ptr + com_begin, ' ', com_end + 3);
+		com_end = mlx_int_str_str(ptr + com_beg + 2, "\n", size - com_beg - 2);
+		memset(ptr + com_beg, ' ', com_end + 3);
+		com_beg = mlx_int_str_str_cote(ptr, "//", size);
 	}
 }
