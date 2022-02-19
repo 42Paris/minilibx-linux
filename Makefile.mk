4b58aeff2e3e0f711f0da41433fb5c168ec27fb0
Luis Geniole <luis.geniole@gmail.com>
2022-02-18 22:22:39 -0300

    Reuse mlx_new_window code.

--- Makefile.mk
+++ Makefile.mk
@@ -1,9 +1,9 @@
 ##
 ## Makefile for MiniLibX in /home/boulon/work/c/raytraceur/minilibx
-## 
+##
 ## Made by Olivier Crouzet
 ## Login   <ol@epitech.net>
-## 
+##
 ## Started on  Tue Oct  5 15:56:43 2004 Olivier Crouzet
 ## Last update Tue May 15 15:41:20 2007 Olivier Crouzet
 ##
@@ -32,7 +32,7 @@ SRC	= mlx_init.c mlx_new_window.c mlx_pixel_put.c mlx_loop.c \
 	mlx_xpm.c mlx_int_str_to_wordtab.c mlx_destroy_window.c \
 	mlx_int_param_event.c mlx_int_set_win_event_mask.c mlx_hook.c \
 	mlx_rgb.c mlx_destroy_image.c mlx_mouse.c mlx_screen_size.c \
-	mlx_new_fullscreen_window.c mlx_destroy_display.c
+	mlx_destroy_display.c
 
 OBJ_DIR = obj
 OBJ	= $(addprefix $(OBJ_DIR)/,$(SRC:%.c=%.o))
--- mlx_new_window.c
+++ mlx_new_window.c
@@ -1,13 +1,12 @@
 /*
-** mlx_new_window.c for MiniLibX in 
-** 
+** mlx_new_window.c for MiniLibX in
+**
 ** Made by Charlie Root
 ** Login   <ol@epitech.net>
-** 
+**
 ** Started on  Mon Jul 31 17:29:02 2000 Charlie Root
 ** Last update Thu Oct  4 15:44:43 2001 Charlie Root
 */
-
 
 /*
 ** We do not use White/BlackPixel macro, TrueColor Visual make sure
@@ -16,14 +15,11 @@
 ** With mlx_int_wait_first_expose, no flush is needed.
 */
 
-#include	"mlx_int.h"
+#include "mlx_int.h"
 
-
-void	*mlx_new_window(t_xvar *xvar,int size_x,int size_y,char *title)
+static XSetWindowAttributes	get_default_attributes(t_xvar *xvar)
 {
-	t_win_list				*new_win;
 	XSetWindowAttributes	xswa;
-	XGCValues				xgcv;
 
 	xswa.background_pixel = 0;
 	xswa.border_pixel = -1;
@@ -33,21 +29,33 @@ void	*mlx_new_window(t_xvar *xvar,int size_x,int size_y,char *title)
 		KeyPressMask | KeyReleaseMask | StructureNotifyMask;
 	*/
 	/* xswa.event_mask = ExposureMask; */
-	xswa.event_mask = 0xFFFFFF;	/* all events */
+	xswa.event_mask = 0xFFFFFF; /* all events */
+	return (xswa);
+}
+
+static void	*allocate_window(void)
+{
+	t_win_list	*new_win;
+
 	if (!(new_win = malloc(sizeof(*new_win))))
 		return ((void *)0);
-	new_win->window = XCreateWindow(xvar->display,xvar->root,0,0,size_x,size_y,
-					0,CopyFromParent,InputOutput,xvar->visual,
-					CWEventMask|CWBackPixel|CWBorderPixel|
-					CWColormap,&xswa);
-	mlx_int_anti_resize_win(xvar,new_win->window,size_x,size_y);
-	XStoreName(xvar->display,new_win->window,title);
-	XSetWMProtocols(xvar->display, new_win->window, &(xvar->wm_delete_window), 1);
+	return (new_win);
+}
+
+static void	*set_configs(t_xvar *xvar, t_win_list *new_win, int size_x,
+		int size_y, char *title)
+{
+	XGCValues	xgcv;
+
+	mlx_int_anti_resize_win(xvar, new_win->window, size_x, size_y);
+	XStoreName(xvar->display, new_win->window, title);
+	XSetWMProtocols(xvar->display, new_win->window, &(xvar->wm_delete_window),
+			1);
 	xgcv.foreground = -1;
 	xgcv.function = GXcopy;
 	xgcv.plane_mask = AllPlanes;
-	new_win->gc = XCreateGC(xvar->display,new_win->window,
-				GCFunction|GCPlaneMask|GCForeground,&xgcv);
+	new_win->gc = XCreateGC(xvar->display, new_win->window,
+			GCFunction | GCPlaneMask | GCForeground, &xgcv);
 	new_win->next = xvar->win_list;
 	xvar->win_list = new_win;
 	/*
@@ -56,7 +64,37 @@ void	*mlx_new_window(t_xvar *xvar,int size_x,int size_y,char *title)
 	new_win->expose_hook = mlx_int_do_nothing;
 	*/
 	bzero(&(new_win->hooks), sizeof(new_win->hooks));
-	XMapRaised(xvar->display,new_win->window);
-	mlx_int_wait_first_expose(xvar,new_win->window);
+	XMapRaised(xvar->display, new_win->window);
+	mlx_int_wait_first_expose(xvar, new_win->window);
 	return (new_win);
+}
+
+void	*mlx_new_window(t_xvar *xvar, int size_x, int size_y, char *title)
+{
+	t_win_list				*new_win;
+	XSetWindowAttributes	xswa;
+
+	xswa = get_default_attributes(xvar);
+	new_win = allocate_window();
+	new_win->window = XCreateWindow(xvar->display, xvar->root, 0, 0, size_x,
+			size_y, 0, CopyFromParent, InputOutput, xvar->visual,
+			CWEventMask | CWBackPixel | CWBorderPixel | CWColormap, &xswa);
+	return (set_configs(xvar, new_win, size_x, size_y, title));
+}
+
+void	*mlx_new_fullscreen_window(t_xvar *xvar, int *size_x, int *size_y,
+		char *title)
+{
+	t_win_list				*new_win;
+	XSetWindowAttributes	xswa;
+
+	xswa = get_default_attributes(xvar);
+	xswa.override_redirect = 1;
+	new_win = allocate_window();
+	mlx_get_screen_size(xvar, size_x, size_y);
+	new_win->window = XCreateWindow(xvar->display, xvar->root, 0, 0, *size_x,
+			*size_y, 0, CopyFromParent, InputOutput, xvar->visual,
+			CWEventMask | CWBackPixel | CWBorderPixel | CWColormap | CWOverrideRedirect,
+			&xswa);
+	return (set_configs(xvar, new_win, *size_x, *size_y, title));
 }
