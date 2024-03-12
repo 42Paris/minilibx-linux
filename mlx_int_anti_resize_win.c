ab12e7b2492e5f2436405d8b1a68e6e97775170d
QuentinThierry <qthierry2@gmail.com>
2024-03-12 16:28:06 +0100

    [FIX] fixed valgrind error 'SUSE11 writev uninit padding' caused by an uninitialized stack-allocated variable 'XSizeHints hints'

--- mlx_int_anti_resize_win.c
+++ mlx_int_anti_resize_win.c
@@ -13,7 +13,7 @@
 
 int	mlx_int_anti_resize_win(t_xvar *xvar,Window win,int w,int h)
 {
-  XSizeHints    hints;
+  XSizeHints    hints = {0};
   long		toto;
   
   XGetWMNormalHints(xvar->display,win,&hints,&toto);
